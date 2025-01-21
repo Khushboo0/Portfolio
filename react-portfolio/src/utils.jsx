85907723a8e05ee4e9e0ed5421d80687d8b62632
Khushboo <patelkhushboo057@gmail.com>
2025-01-21 23:38:59 +0530

    navigation is responsive and completed close icon issue is opened

--- react-portfolio/src/utils.jsx
+++ react-portfolio/src/utils.jsx
@@ -1,3 +1,7 @@
 export const getImageUrl = (path) => {
-    return new URL(`/assets/${path}`, import.meta.url).href;
-  };
+  if (!path) {
+    console.error("Path is undefined or empty.");
+    return "";
+  }
+  return `/src/assets/${path}`;
+};
